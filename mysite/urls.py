9a10f614f9a5f95f017c885564f833b2ed14ffd2
ridvan <[email>
2023-06-13 16:01:52 +0300

    changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include, url
+from django.urls import include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
+    re_path(r'', include('blog.urls')),
 ]
