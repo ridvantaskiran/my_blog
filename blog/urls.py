bc3dc96df90fbd4a7d7bfff0f4740ea3d57fd664
ridvan <[email>
2023-07-04 15:44:10 +0300

    'changes'

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
-from django.urls import re_path
+from django.urls import re_path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    re_path("admin/", admin.site.urls),
+    re_path("accounts/", include("django.contrib.auth.urls")),
     re_path(r'^$', views.post_list, name='post_list'),
     re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     re_path(r'^post/new/$', views.post_new, name='post_new'),
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,11 +123,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
